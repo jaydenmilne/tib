426e8dda0a1ab6b6bab74ae352a7cb5566697d26
Jayden Milne <jaydenmilne@users.noreply.github.com>
2021-08-13 11:24:05 -0600

    start fresh

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-mod executor;
-mod interpreter;
-mod lexer;
-mod parser;
-
-use std::env;
-use std::fs;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() == 1 {
-        println!("tib {} (c) 2020 Jayden Milne", env!("CARGO_PKG_VERSION"));
-        interpreter::interpret_repl();
-    } else {
-        if args[1] == "--help" || args[1] == "-h" {
-            println!("tib {} (c) 2020 Jayden Milne", env!("CARGO_PKG_VERSION"));
-            println!("Usage: tib [filename, optional]");
-            println!("If no filename is provided, you will enter a REPL");
-            println!("Special thanks to TI-BASIC Developer (tibasicdev.wikidot.com)");
-            return;
-        }
-
-        let filename = &args[1];
-        match fs::read_to_string(filename) {
-            Ok(file) => interpreter::interpret_file(&file),
-            Err(error) => panic!("Could not open the file {}, error is {:?}", filename, error),
-        };
-    }
+    println!("Hello, world!");
 }
